4ba47846b79afefdc697bf6614be161b2d9e2690
santi guerrero <santiagogzapiain@gmail.com>
2026-03-10 13:47:23 -0600

    v2

--- clases/herencia1/auto_particular.py
+++ clases/herencia1/auto_particular.py
@@ -1,16 +1,25 @@
-from clases.herencia1.vehiculo import Vehiculo
-
-class AutoParticular(Vehiculo):
-
-    def __init__(self, matricula, modelo, potenciaCV, numeroPuertas):
-        super().__init__(matricula, modelo, potenciaCV)
-        self.numeroPuertas = numeroPuertas
-
+from clases.herencia2.persona import Persona
+class AutoParticular(Persona):
+    def __init__(self, clave, nombre, edad, marca, color, placa):
+        super().__init__(clave, nombre, edad)
+        
+        self.marca = marca
+        self.color = color
+        self.placa = placa
+        
     def __str__(self):
-        return super().__str__() + " " + str(self.numeroPuertas)
-
-    def abrirPuertas(self):
-        print("Abriendo puertas...")
-
-    def cerrarPuertas(self):
-        print("Cerrando puertas...")
+        return super().__str__()+""+self.marca+" "+self.color+" "+self.placa
+    
+    
+    def subirseAuto(self):
+        print("Subiendo al auto")
+        
+    def arrancarAuto(self):
+        print("Encendiendo la radio")
+        print("Arrancando el auto")
+    
+    def llegarDestino(self):
+        print("Llegando al destino")
+        
+    def bajandoAuto(self):
+        print("Bajando del auto")
--- main.py
+++ main.py
@@ -1,11 +1,23 @@
-from clases.herencia1.taxi import Taxi 
+from clases.herencia1.taxi import Taxi
+from clases.herencia1.auto_particular import AutoParticular
+
 def main():
-    coche = Taxi("123-GTO","Versa",1000,"123-a") 
-    print(coche) 
-    coche.encender() 
-    coche.subirPasajeros() 
-    coche.acelerar() 
-    coche.frenar() 
-    coche.cobrarCuota() 
-if __name__ == '__main__': 
-        main()
+    coche = Taxi("123-3","la ferrari",3500,"384-a")
+    print("************")
+    print(coche)
+    coche.encender()
+    coche.subirPasajeros()
+    coche.acelerar()
+    coche.frenar()
+    coche.cobrarCuota()
+
+    ap= AutoParticular("123","santiago",19,"bmw","blanco","234-A")
+    print(ap)
+    
+    ap.subirseAuto()
+    ap.arrancarAuto()
+    ap.llegarDestino()
+    ap.bajandoAuto()
+
+if __name__=='__main__':
+    main()
